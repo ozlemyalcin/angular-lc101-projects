1e33555cad9b41b6b51442d8b3592b21f12750af
ozlemyalcin <omersam33@gmail.com>
2020-12-07 16:38:54 -0600

    last

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Photo/Image';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
   image2 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfVP6J-OuD3MhgAtij8sN9Y2l4sLzVe7-LHw&usqp=CAU';
   image3 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRMZy5cJZomK9EtZDToyCVvuC_ptC_egae9w&usqp=CAU';
--- lesson1/exercises/src/app/page-title/page-title.component.ts
+++ lesson1/exercises/src/app/page-title/page-title.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page-title.component.css']
 })
 export class PageTitleComponent implements OnInit {
-  title='Page Title';
+  title='Angular ';
   constructor() { }
 
   ngOnInit() {
