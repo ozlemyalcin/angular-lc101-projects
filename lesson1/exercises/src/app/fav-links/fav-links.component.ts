6eb0abb5d0790e55d9a9a1250eee6612509cb346
ozlemyalcin <omersam33@gmail.com>
2020-12-07 15:59:09 -0600

    finish part2

---
+++ lesson1/exercises/src/app/fav-links/fav-links.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fav-links',
+  templateUrl: './fav-links.component.html',
+  styleUrls: ['./fav-links.component.css']
+})
+export class FavLinksComponent implements OnInit {
+  favLinks=['https://calendar.google.com/calendar/u/0/r','https://learn.launchcode.org/courses/271']
+  
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class FavPhotosComponent implements OnInit {
   photosTitle = 'Photo/Image Section Title Here';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfVP6J-OuD3MhgAtij8sN9Y2l4sLzVe7-LHw&usqp=CAU';
+  image3 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRMZy5cJZomK9EtZDToyCVvuC_ptC_egae9w&usqp=CAU';
 
   constructor() { }
 
