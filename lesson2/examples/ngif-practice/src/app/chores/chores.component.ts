5e488befe8b98c9ce7932b28f0a3b86176b1eb27
ozlemyalcin <omersam33@gmail.com>
2020-12-10 11:57:38 -0600

    finish the class ngif

--- lesson2/examples/ngif-practice/src/app/chores/chores.component.ts
+++ lesson2/examples/ngif-practice/src/app/chores/chores.component.ts
@@ -6,8 +6,8 @@ import { Component, OnInit } from '@angular/core';
    styleUrls: ['./chores.component.css']
 })
 export class ChoresComponent implements OnInit {
-   chores = ['Clean Kitchen','Call mom','Work prep work','no worry'];
-   finishedChores = ['call mom'];
+   chores = ['Clean Kitchen','Buy groceries'];
+   finishedChores = ['Laundry','Trash takeout','Buy gifts','Dust tables'];
 
    targetImage = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
 
