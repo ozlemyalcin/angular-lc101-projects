ce8820ee8a3c08314374cc87954e05e01acec77d
ozlemyalcin <omersam33@gmail.com>
2020-12-10 14:16:55 -0600

    angular part5-6

--- lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
+++ lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
@@ -12,4 +12,12 @@ export class MovieListComponent implements OnInit {
 
    ngOnInit() {
    }
+
+   addMovie (newTitle: string){
+      if(!this.movies.includes(newTitle)){
+         this.movies.push(newTitle);
+
+      }
+      
+   }
 }
